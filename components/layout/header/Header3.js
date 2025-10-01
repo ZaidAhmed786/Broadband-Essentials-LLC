17949161b0e25e96984cc3ae70cfe7c49e10f656
Abdur Rafay <158170390+Rafaykz@users.noreply.github.com>
2025-10-01 09:24:17 +0500

    upadte email

--- components/layout/header/Header3.js
+++ components/layout/header/Header3.js
@@ -20,7 +20,7 @@ export default function Header1({
                             <ul className="contact-list">
                                 <li>
                                     <i className="far fa-envelope" />
-                                    <Link href="mailto:sales@broadbandessentials.site" className="link">support@zolaraconsulting.com </Link>
+                                    <Link href="mailto:sales@broadbandessentials.site" className="link">contact@broadbandessentials.site </Link>
                                 </li>
                                 <li>
                                     <i className="fa-solid fa-phone-volume" />
